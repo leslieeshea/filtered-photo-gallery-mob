a4f53211baa17da882be02bf94ac279e161b912d
Leslie Shea <leslieyshea@gmail.com>
2019-02-26 14:09:50 -0800

    finished writing makeImageTemplate function and moved to own file

--- test/make-image-template.test.js
+++ test/make-image-template.test.js
@@ -1,25 +1,19 @@
 const test = QUnit.test;
 import images from '../data/images.js';
+import makeImageTemplate from '../src/make-image-template.js';
 
-function makeImageTemplate(image) {
-    const html = /*html*/`
-<li>
-                <h2>${image.title}</h2>
-                <img src="${image.url}">
-            </li>`;
-    return html;
-}
+
 
 test('Test that image list template is correct', assert => {
 //arrange
-let image = images[0];
-const html = /*html*/`
+    let image = images[0];
+    const html = /*html*/`
 <li>
                 <h2>UniWhal</h2>
                 <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
             </li>`;
 //act
-const result = makeImageTemplate(image);
+    const result = makeImageTemplate(image);
 //assert
-assert.equal(result, html);
+    assert.htmlEqual(result, html);
 });
